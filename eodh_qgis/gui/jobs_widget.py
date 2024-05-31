442018a5d70f973642e0a148d8d68dd8258ae339
Dusan Figala <dusan@oxidian.com>
2024-05-31 13:04:15 +0200

    Populate tables with dummy data

--- eodh_qgis/gui/jobs_widget.py
+++ eodh_qgis/gui/jobs_widget.py
@@ -12,3 +12,28 @@ class JobsWidget(QtWidgets.QWidget, FORM_CLASS):
         """Constructor."""
         super(JobsWidget, self).__init__(parent)
         self.setupUi(self)
+        self.table: QtWidgets.QTableWidget
+        self.details_button: QtWidgets.QPushButton
+        self.populate_table_dummy()
+        self.table.cellClicked.connect(self.handle_table_click)
+
+    def populate_table_dummy(self):
+        data = [
+            ("job1", "process1", "completed", "2024-05-31 10:36:56"),
+            ("job2", "process2", "accepted", "2024-05-31 10:36:56"),
+            ("job3", "process1", "completed", "2024-05-31 10:36:56"),
+        ]
+        self.table.setRowCount(len(data))
+        self.table.setColumnCount(len(data[0]))
+        self.table.setHorizontalHeaderLabels(["ID", "Process ID", "Status", "Created"])
+        for row_index, row_data in enumerate(data):
+            for col_index, col_data in enumerate(row_data):
+                self.table.setItem(
+                    row_index, col_index, QtWidgets.QTableWidgetItem(col_data)
+                )
+
+        self.table.show()
+
+    def handle_table_click(self, row, col):
+        print(row, col)
+        self.details_button.setEnabled(True)
--- eodh_qgis/gui/workflows_widget.py
+++ eodh_qgis/gui/workflows_widget.py
@@ -20,10 +20,12 @@ class WorkflowsWidget(QtWidgets.QWidget, FORM_CLASS):
         self.new_button: QtWidgets.QPushButton
         self.edit_button: QtWidgets.QPushButton
         self.execute_button: QtWidgets.QPushButton
+        self.table: QtWidgets.QTableWidget
 
         self.new_button.clicked.connect(self.handle_new)
         self.edit_button.clicked.connect(self.handle_new)
         self.execute_button.clicked.connect(self.handle_execute)
+        self.populate_table_dummy()
 
     def handle_new(self):
         editor = WorkflowEditorWidget(parent=self.parent())
@@ -34,3 +36,20 @@ class WorkflowsWidget(QtWidgets.QWidget, FORM_CLASS):
         executor = WorkflowExecutorWidget(parent=self.parent())
         self.parent().addWidget(executor)
         self.parent().setCurrentWidget(executor)
+
+    def populate_table_dummy(self):
+        data = [
+            ("id1", "title1", "description1"),
+            ("id2", "title2", "description2"),
+            ("id3", "title3", "description3"),
+        ]
+        self.table.setRowCount(len(data))
+        self.table.setColumnCount(len(data[0]))
+        self.table.setHorizontalHeaderLabels(["ID", "Title", "Description"])
+        for row_index, row_data in enumerate(data):
+            for col_index, col_data in enumerate(row_data):
+                self.table.setItem(
+                    row_index, col_index, QtWidgets.QTableWidgetItem(col_data)
+                )
+
+        self.table.show()
